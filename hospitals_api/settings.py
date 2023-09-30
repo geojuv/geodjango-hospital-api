e435a5cb05933737ce4cb655a88789a6b7eb8f71
J wil <geojuv@gmail.com>
2023-09-29 21:33:25 -0400

    chore: set up docker and postgis

--- hospitals_api/settings.py
+++ hospitals_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+env= environ.Env(DEBUG=(bool,False))
+environ.Env.read_env(env_file=".env")
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-*7%cvv@hz$bdi8spg_6i2qit+dzye+4+)37%yqkox63+ehm1g'
+# SECRET_KEY = '-*7%cvv@hz$bdi8spg_6i2qit+dzye+4+)37%yqkox63+ehm1g'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -73,16 +80,21 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'hospitals_api.wsgi.application'
+WSGI_APPLICATION = 'hospitals_api.wsgi.application' 
+
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+ 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": env("POSTGRES_DBNAME"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASS"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
     }
 }
 
